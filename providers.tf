f53636b4ca38dbd8d29123999ad340da8e4d9128
b-abh-007 <109807058+b-abh-007@users.noreply.github.com>
2022-11-30 20:45:00 +0200

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  shared_credentials_files = ["~/.aws/credentials"]
+#   profile                  = "admin"
+  region = "me-central-1"
+} 
