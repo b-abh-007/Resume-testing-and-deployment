d20af03e9ee2c13bfd24c935c421e2f6bd82788f
b-abh-007 <109807058+b-abh-007@users.noreply.github.com>
2022-12-05 14:47:22 +0200

    Update main_S3.tf

--- main_S3.tf
+++ main_S3.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket_policy" "policy" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "test-rwh-3"
+  bucket = "test-rwh-4"
 }
 
 
@@ -23,7 +23,7 @@ resource "aws_s3_bucket_acl" "acl" {
 }
  
 resource "aws_s3_object" "folder-object" {
-  bucket = "test-rwh-3"
+  bucket = "test-rwh-4"
   key    = "Resume/"
   # source       = "../Resume/"
   content_type = "application/x-directory"
@@ -51,7 +51,7 @@ resource "aws_s3_object" "folder-object" {
 resource "aws_s3_object" "html-files" {
   for_each = fileset("Resume/", "*.html")
 
-  bucket       = "test-rwh-3"
+  bucket       = "test-rwh-4"
   key          = "Resume/${each.value}"
   source       = "Resume/${each.value}"
   content_type = "text/html"
@@ -63,7 +63,7 @@ resource "aws_s3_object" "html-files" {
 resource "aws_s3_object" "css-files" {
   for_each = fileset("Resume/", "*.css")
 
-  bucket       = "test-rwh-3"
+  bucket       = "test-rwh-4"
   key          = "Resume/${each.value}"
   source       = "Resume/${each.value}"
   content_type = "text/css"
@@ -75,7 +75,7 @@ resource "aws_s3_object" "css-files" {
 resource "aws_s3_object" "js-files" {
   for_each = fileset("Resume/", "*.js")
 
-  bucket       = "test-rwh-3"
+  bucket       = "test-rwh-4"
   key          = "Resume/${each.value}"
   source       = "Resume/${each.value}"
   content_type = "application/javascript"
@@ -87,7 +87,7 @@ resource "aws_s3_object" "js-files" {
 resource "aws_s3_object" "image-files" {
   for_each = fileset("Resume/", "*.png")
 
-  bucket       = "test-rwh-3"
+  bucket       = "test-rwh-4"
   key          = "Resume/${each.value}"
   source       = "Resume/${each.value}"
   content_type = "image/jpeg"
@@ -99,7 +99,7 @@ resource "aws_s3_object" "image-files" {
 resource "aws_s3_object" "image-files-2" {
   for_each = fileset("Resume/", "*.jfif")
 
-  bucket       = "test-rwh-3"
+  bucket       = "test-rwh-4"
   key          = "Resume/${each.value}"
   source       = "Resume/${each.value}"
   content_type = "image/jpeg"
@@ -111,7 +111,7 @@ resource "aws_s3_object" "image-files-2" {
 # resource "aws_s3_object" "image-files-test" {
 # #   for_each = fileset("Resume/", "*.jfif")
 
-#   bucket       = "test-rwh-3"
+#   bucket       = "test-rwh-4"
 #   key          = "Resume/house.jfif"
 #   source       = "Resume/house.jfif"
 #   content_type = "image/jpeg"
