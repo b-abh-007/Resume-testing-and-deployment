41611a70929f23ac4e5f3430f50491b409f7a856
b-abh-007 <109807058+b-abh-007@users.noreply.github.com>
2022-12-15 00:51:50 +0200

    Update main_lambda.tf

--- main_lambda.tf
+++ main_lambda.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "resume-lambda-function" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = local.lambda_zip_location
-  function_name = "resume-lambda-function"
+  function_name = "resume-lambda-function-boto3"
   role          = aws_iam_role.test-lambda-role.arn
 
   handler = "lambda-function.handler"
@@ -15,13 +15,13 @@ resource "aws_lambda_function" "resume-lambda-function" {
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
   # source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-  runtime = "nodejs16.x"
+  runtime = "python3.9"
 
 }
 
 data "archive_file" "lambda-archive" {
   type        = "zip"
-  source_file = "lambda-function.js"
+  source_file = "lambda-function.py"
   output_path = local.lambda_zip_location
 }
 
