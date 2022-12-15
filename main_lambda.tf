b147fe0a591e1cf14a6e665ed15694bf8cb0d578
b-abh-007 <109807058+b-abh-007@users.noreply.github.com>
2022-12-15 19:01:17 +0200

    Update main_lambda.tf

--- main_lambda.tf
+++ main_lambda.tf
@@ -2,20 +2,11 @@ locals { #declaring local variables
   lambda_zip_location = "output/lambda-function.zip"
 }
 
-data "archive_file" "lambda-archive" {
-  type        = "zip"
-  source_file = "lambda-function.js"
-  output_path = local.lambda_zip_location
-}
-
 resource "aws_lambda_function" "resume-lambda-function" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  depends_on = [
-  data.archive_file.lambda-archive
-  ]
   filename      = local.lambda_zip_location
-  function_name = "resume-lambda-function"
+  function_name = "resume-lambda-function-boto3"
   role          = aws_iam_role.test-lambda-role.arn
 
   handler = "lambda-function.handler"
@@ -24,11 +15,15 @@ resource "aws_lambda_function" "resume-lambda-function" {
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
   # source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-  runtime = "nodejs16.x"
+  runtime = "python3.9"
 
-} 
+}
 
-
+data "archive_file" "lambda-archive" {
+  type        = "zip"
+  source_file = "lambda-function.py"
+  output_path = local.lambda_zip_location
+}
 
 resource "aws_lambda_permission" "allow_api_gateway" {
   action        = "lambda:InvokeFunction"
